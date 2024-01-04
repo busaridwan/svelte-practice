33e899dfb29819d2c74df272fc63d6d3124ee423
Muhammed Busari Ridwan <busaridwan@gmail.com>
2024-01-04 22:38:14 +0100

    done with practise

--- src/App.svelte
+++ src/App.svelte
@@ -1,66 +1,42 @@
 <script>
-	export let firstName;
-	let lastName = "Busari";
-	$: name = lastName + ' ' + firstName;
-	let color = "blue";
-	let showText = false;
-	const toggle = () => {
-		color = color === 'blue' ? 'purple' : 'blue';
-		showText = !showText;
-		users = [...users, {id: 4, name: 'Baba'}]
-	};
-	let users = [
-		{
-			id: 1,
-			name: 'Ridwan'
-		},
-		{
-			id: 2,
-			name: 'Busari'
-		},
-		{
-			id: 3,
-			name: 'Naheem'
-		}
-
-	]
+	import FeedbackForm from "./components/FeedbackForm.svelte";
+	import FeedbackList from "./components/FeedbackList.svelte";
+	import FeedbackStats from "./components/FeedbackStats.svelte";
+// 	let feedback = [
+//   {
+//     id: 1,
+//     rating: 10,
+//     text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. consequuntur vel vitae commodi alias voluptatem est voluptatum ipsa quae.',
+//   },
+//   {
+//     id: 2,
+//     rating: 9,
+//     text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. consequuntur vel vitae commodi alias voluptatem est voluptatum ipsa quae.',
+//   },
+//   {
+//     id: 3,
+//     rating: 8,
+//     text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. consequuntur vel vitae commodi alias voluptatem est voluptatum ipsa quae.',
+//   },
+// ]
+// const deleteFeedback = (e) => {
+// 	console.log(e.detail);
+// 	const itemId = e.detail;
+// 	feedback = feedback.filter((item) => item.id != itemId);
+// }
+// $: count = feedback.length;
+// $: average = feedback.reduce((a, {rating}) => a + rating, 0) / feedback.length;
 
 </script>
 
-<main>
-	<h1 style="color: {color};">Hello {name}!</h1>
-	{#if showText}
-		<p>Visit the <a href="/">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-	{:else}	
-	<p>No Text</p>
-	{/if}
-	
-	<button on:click={() => color = 'purple'}>Click</button>
-	<button on:click={toggle}>Click</button>
-	{#each users as user (user.id) }
-		<h3>{user.id}: {user.name}</h3>
-	{/each}
+<main class="container">
+	<FeedbackForm />
+	<FeedbackStats />
+	<!-- <FeedbackStats {count} {average} /> -->
+	<!-- <FeedbackList feedback={feedback}/> -->
+	<FeedbackList />
+	<!-- <FeedbackList {feedback} on:delete-feedback={deleteFeedback}/> -->
+	<!-- Count: {count}
+	Average: {average} -->
 
 </main>
-
-<style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
-</style>
